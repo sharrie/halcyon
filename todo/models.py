f2d53892a14327e28e0ae70ae922cd4c0954ef8b
Conrad Fay <conradfay@conradfay-M14xR2-Ubuntu.(none)>
2012-11-26 23:47:39 -0800

    added models.py and ENV

--- todo/models.py
+++ todo/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.contrib import admin
 
 # Create your models here.
+
+class Item(models.Model):
+    name = models.CharField(max_length=255)
+    created = models.DateTimeField(auto_now_add=True)
+    done = models.BooleanField(default=False)
+    hidden = models.BooleanField(default=False)
+
+    def __unicode__(self):
+        return self.name
+
+class ItemAdmin(admin.ModelAdmin):
+    list_display = ["name","created","done","hidden"]
+    search_fields = ["name"]
+
+admin.site.register(Item, ItemAdmin)
