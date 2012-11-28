ae9bf2a70196c47c8c1d50561da0304519f560ad
Conrad Fay <conradfay@conradfay-M14xR2-Ubuntu.(none)>
2012-11-28 01:06:28 -0800

    added views.py and index.html template

--- todo/views.py
+++ todo/views.py
@@ -1,1 +1,18 @@
+from django.http import HttpResponse
+from django.template import Context, loader
+
+from todo.models import Item
+
 # Create your views here.
+
+def index(request):
+    return HttpResponse("Hello world :(")
+    '''
+    todo_list = Item.objects.order_by('-name')[:5]
+    template = loader.get_template('index.html')
+    context = Context({
+            'todo_list': todo_list,
+            })
+    output = '\n, '.join([t.name for t in todo_list])
+    return HttpResponse(template.render(context))
+    '''
