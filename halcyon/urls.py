98d99b890f0c2267802a44e7340e807a3cecb131
Conrad Fay <conradfay@conradfay-AO756-elementary.(none)>
2013-02-16 00:34:34 -0800

    functional new_todo form.

--- halcyon/urls.py
+++ halcyon/urls.py
@@ -8,16 +8,17 @@ urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'halcyon.views.home', name='home'),
     # url(r'^halcyon/', include('halcyon.foo.urls')),
+    url(r'^$', 'todo.views.index'),
 
     url(r'^login/$', 'django.contrib.auth.views.login'),
 
-    url(r'^todo/todo/', 'todo.views.todo'),
+    url(r'^todo/$', 'todo.views.index'),
+    url(r'^list-todo/$', 'todo.views.todo'),
+    url(r'^todo/new/$', 'todo.views.new_todo'),
 
     # Uncomment the admin/doc line below to enable admin documentation:
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
     # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
-
-    url(r'^todo/', include('todo.urls'))
 )
--- todo/views.py
+++ todo/views.py
@@ -2,21 +2,34 @@ from django.http import HttpResponse, HttpResponseRedirect
 from django.template import Context, loader
 from django.shortcuts import render
 from django.contrib.auth import authenticate, login
+from django.shortcuts import redirect
 
-
+from django.contrib.auth.models import *
 from todo.models import Item
 from todo.forms import TodoForm
 
 # Create your views here.
 
 def index(request):
-    #return HttpResponse("Hello world :(")
-    
+
     todo_list = Item.objects.all()[:5]
 
     context = {"todo_list": todo_list}
 
     return render(request, 'index.html', context)
+
+
+def todo(request):
+
+    if request.method == 'GET':
+        todo_list = Item.objects.all()[:5]
+
+        context = {"todo_list": todo_list}
+
+    if request.method == 'POST':
+        pass
+
+    return render(request, 'todo.html', context)
 
 
 def login(request):
@@ -41,29 +54,19 @@ def login(request):
                 pass
                 # Return an 'invalid login' error message.
 
-
-def list_todo(request):
-    todo_list = Item.objects.all()[:5]
-
-    context = {"todo_list": todo_list}
-
-    return render(request, 'index.html', context)
-    
-
 def new_todo(request):
+
     if request.method == 'GET':
-        form = TodoForm()
+        return render(request, 'new-todo.html')
 
     if request.method == 'POST':
-        form = TodoForm(request.POST)
-        if form.is_valid():
-            form_name = form.cleaned_data['name']
-            form_description = form.cleaned_data['description']
-            form_date_due = form.cleaned_data['date_due']
-            i = Item(name = form_name, description = form_description, date_due = form_date_due)
-            i.save()
-            return HttpResponseRedirect('/admin/')
-        else:
-            form = TodoForm()
+        todo_name = request.POST.get('todo_name')
+        todo_description = None
+        if request.POST.get('todo_description'):
+            todo_description = request.POST.get('todo_description')
 
-    return render(request, 'todo.html', { 'form': form })
+        t = Item(name=todo_name, description=todo_description, user=User.objects.get(username='admin'))
+        t.save()
+
+        return redirect('/')
+
